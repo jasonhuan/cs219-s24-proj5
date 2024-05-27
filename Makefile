f92ed4666821fcca74dbe6f7891d0286b11d298c
jasonhuan <sino@g.ucla.edu>
2024-05-27 15:32:48 -0700

    working gpufit compilation

--- Makefile
+++ Makefile
@@ -4,8 +4,12 @@ CLASSES=
 
 all: mp_solver
 
+gpufit: mp_solver_gpufit
+
 mp_solver: $(CLASSES)
 	$(CXX) -o $@ $@.cpp $(CXXFLAGS)
+
+mp_solver_gpufit: /usr/bin/c++ -O3 -DNDEBUG -std=gnu++14 mp_solver.cpp -o ./mp_solver_gpufit  -Wl,-rpath,/home/shared/gpufit-build/Gpufit /usr/local/lib/libGpufit.so /usr/lib/x86_64-linux-gnu/libcudart_static.a -ldl /usr/lib/x86_64-linux-gnu/librt.a -lgsl -lm -lgslcblas
 
 clean:
 	rm -rf *.o mp_solver
--- mp_solver.cpp
+++ mp_solver.cpp
@@ -9,7 +9,8 @@
 #include <gsl/gsl_multifit_nlinear.h>
 #include <immintrin.h>
 #include "./mp_solver.h"
-#include "/usr/local/include/gpufit/gpufit.h"
+//#include "/usr/local/include/gpufit/gpufit.h"
+#include "/home/shared/Gpufit/Gpufit/gpufit.h"
 
 #include <iostream>
 #include <fstream>
@@ -192,13 +193,99 @@ int mp_solver(mp_config_t *mp_config, mp_profile_t *mp_profile)
   return 0;
 }
 
+void simple_example()
+{
+	/*
+		This example demonstrates a simple, minimal program containing all 
+		of the required parameters for a call to the Gpufit function.  The example 
+		can be built and executed within the project environment. Please note that 
+		this code does not actually do anything other than make a single call to 
+		gpufit().
+
+		In the first section of the code, the *model ID* is set, memory space for 
+		initial parameters and data values is allocated, the *fit tolerance* is set, 
+		the *maximum number of iterations* is set, the *estimator ID* is set, and 
+		the *parameters to fit array* is initialized.  Note that in most applications, 
+		the data array will already exist and it will be unnecessary to allocate 
+		additional space for data.  In this example, the *parameters to fit* array 
+		is initialized to all ones, indicating that all model parameters should be 
+		adjusted in the fit.
+	*/
+
+    /*************** definition of input and output parameters  ***************/
+
+	// number of fits, number of points per fit
+	std::size_t const n_fits = 10;
+	std::size_t const n_points_per_fit = 10;
+
+	// model ID and number of model parameters
+	int const model_id = GAUSS_1D;
+	std::size_t const n_model_parameters = 4;
+
+	// initial parameters
+	std::vector< REAL > initial_parameters(n_fits * n_model_parameters);
+
+	// data
+	std::vector< REAL > data(n_points_per_fit * n_fits);
+
+	// tolerance
+	REAL const tolerance = 0.001f;
+
+	// maximum number of iterations
+	int const max_number_iterations = 10;
+
+	// estimator ID
+	int const estimator_id = LSE;
+
+	// parameters to fit (all of them)
+	std::vector< int > parameters_to_fit(n_model_parameters, 1);
+
+	// output parameters
+	std::vector< REAL > output_parameters(n_fits * n_model_parameters);
+	std::vector< int > output_states(n_fits);
+	std::vector< REAL > output_chi_square(n_fits);
+	std::vector< int > output_number_iterations(n_fits);
+
+    /***************************** call to gpufit  ****************************/
+
+	int const status = gpufit
+        (
+            n_fits,
+            n_points_per_fit,
+            data.data(),
+            0,
+            model_id,
+            initial_parameters.data(),
+            tolerance,
+            max_number_iterations,
+            parameters_to_fit.data(),
+            estimator_id,
+            0,
+            0,
+            output_parameters.data(),
+            output_states.data(),
+            output_chi_square.data(),
+            output_number_iterations.data()
+        );
+
+    /****************************** status check  *****************************/
+
+	if (status != ReturnState::OK)
+	{
+		throw std::runtime_error(gpufit_get_last_error());
+	} else {
+		std::cout << "working gpufit status check\n";
+	}
+}
+
+
 int main() {
     // read input
     // mp_config_t mp_config;
     // mp_profile_t mp_profile;
     // mp_config.nof_pilots = 32;
     // mp_config.nof_paths = 3;
-
+    // simple_example(); //test gpufit
 
     std::ifstream file("sample_input.csv");
     if (!file.is_open()) {
@@ -284,5 +371,7 @@ int main() {
 
     // ==================AFTER for loop=====================
     cout << "==================AFTER for loop=====================" << '\n';
+    simple_example(); //test gpufit
     std::cout << "main completed" << std::endl;
 }
+

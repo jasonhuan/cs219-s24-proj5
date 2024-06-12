1296480f90a4cf8f12596f3e69976f6f376ce76e
jasonhuan <sino@g.ucla.edu>
2024-06-12 08:06:11 -0700

    debug

--- mp_solver.cpp
+++ mp_solver.cpp
@@ -199,7 +199,7 @@ int mp_solver(mp_config_t *mp_config, mp_profile_t *mp_profile)
   return 0;
 }
 
-void test_gpufit(float* _initial_parameters, float* _initial_data, float* _output_parameters)
+void test_gpufit(std::vector<float> _initial_parameters, std::vector<float> _initial_data, std::vector<float> _output_parameters)
 {
 	/*
 		This example demonstrates a simple, minimal program containing all 
@@ -387,9 +387,9 @@ int main(int argc, char *argv[]) {
     cout << "==================AFTER for loop=====================" << '\n';
 
     int num_fits;
-    float initial_parameters[num_fits*3*MAX_NOF_PATHS];
-    float initial_data[num_fits*MAX_NOF_PILOTS];
-    float output_parameters[num_fits*3*MAX_NOF_PATHS];
+    std::vector<float> initial_parameters[num_fits*3*MAX_NOF_PATHS];
+    std::vector<float> initial_data[num_fits*MAX_NOF_PILOTS];
+    std::vector<float> output_parameters[num_fits*3*MAX_NOF_PATHS];
 
     test_gpufit(initial_parameters, initial_data, output_parameters); //test gpufit
 
